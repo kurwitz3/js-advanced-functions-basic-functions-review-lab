1514f59a6de52cae1e548d5dfbe3c74e75bead6d
kurwitz3 <kurwitz3@aol.com>
2021-02-04 19:53:10 -0500

    commit tree 67b72c6b4e93119b8ece3b0bb2259fce63b7343c

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective(string = '*'){
+    return function(param = "special"){
+        return `You are ${string}${param}${string}!`
+    }
+}
+
+const Calculator = {
+    add:function(){return 1 +3},
+    subtract:function(){return 1 - 3},
+    multiply:function(){return 1 * 3},
+    divide:function(){return 10 / 5}}
+
+function actionApplyer(integer,array){
+    for (let i = 0; i < array.length; i++){
+      integer = array[i](integer);
+    }
+    return integer;
+}
